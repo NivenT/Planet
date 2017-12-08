2a783e4200c70f50b6004c9a9bef874fdabca363
NivenT <nachenjang@gmail.com>
2017-12-08 15:03:08 -0800

    hexagon has color

--- src/MainGame.cpp
+++ src/MainGame.cpp
@@ -2,11 +2,13 @@
 #include "nta/SystemManager.h"
 #include "nta/ResourceManager.h"
 #include "nta/Vertex.h"
+#include "nta/Random.h"
 
 #include "MainGame.h"
 
 using namespace std;
 using namespace nta;
+using namespace glm;
 
 MainGame::MainGame() {
 }
@@ -15,9 +17,9 @@ MainGame::~MainGame() {
 }
 
 void MainGame::init() {
-    nta::Logger::writeToLog("Initializing main screen...");
+    Logger::writeToLog("Initializing main screen...");
 
-    m_simpleProg = nta::SystemManager::getGLSLProgram("Simple2D");
+    m_simpleProg = SystemManager::getGLSLProgram("Simple2D");
     if (!m_simpleProg->isLinked()) {
         m_simpleProg->addAttribute("pos");
         m_simpleProg->addAttribute("color");
@@ -31,7 +33,7 @@ void MainGame::init() {
 
     m_batch.init();
 		m_pbatch.init();
-    nta::Logger::writeToLog("main screen initialized");
+    Logger::writeToLog("main screen initialized");
 }
 
 void MainGame::update() {
@@ -43,16 +45,18 @@ void MainGame::render() {
     m_batch.begin(); {
 		} m_batch.end();
 
+    static vec4 color(Random::randFloat(), Random::randFloat(), Random::randFloat(), 1.0);
 		m_pbatch.begin(); {
         vector<Vertex2D> verts;
         for (int i = 0; i < 6; i++) {
-            verts.emplace_back(10.f * glm::vec2(cos(i * M_PI/3), sin(i * M_PI/3)));
+            verts.emplace_back(10.f * vec2(cos(i * M_PI/3), sin(i * M_PI/3)), color);
         }
         m_pbatch.addPrimitive(begin(verts), end(verts), 0);
 		} m_pbatch.end();
 		
     m_simpleProg->use(); {
-        glUniformMatrix3fv(m_simpleProg->getUniformLocation("camera"), 1, GL_FALSE, &m_camera.getCameraMatrix()[0][0]);
+        glUniformMatrix3fv(m_simpleProg->getUniformLocation("camera"), 1, GL_FALSE,
+                           &m_camera.getCameraMatrix()[0][0]);
         m_batch.render();
         m_pbatch.render();
 		} m_simpleProg->unuse();
